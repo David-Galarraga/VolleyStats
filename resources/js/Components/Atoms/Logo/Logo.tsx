881e101b060890593daf0258ff79406ed0b8ff63
Ignacio_Galarraga <ignacio.g4l4rr4g4@gmail.com>
2026-09-01 17:33:23 -0300

    feat: componentes, styles globales y scss
    Conflictos solucionados

--- resources/js/Components/Atoms/Logo/Logo.tsx
+++ resources/js/Components/Atoms/Logo/Logo.tsx
@@ -1,5 +1,9 @@
 import styles from './Logo.module.scss';
 
 export default function Logo() {
+<<<<<<< HEAD
     return <span className={styles.logo}>VOLLEYSTATS</span>;
+=======
+    return <span className={styles.logo}>VIVE</span>;
+>>>>>>> db10857 (feat: componentes, styles globales y scss)
 }
