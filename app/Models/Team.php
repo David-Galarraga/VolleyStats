7c2ce52623f86f5a3720b8e77f79ae2413c41c83
Ignacio_Galarraga <ignacio.g4l4rr4g4@gmail.com>
2026-09-10 17:22:05 -0300

    Feat: crud de equipo y entrenador

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    protected $fillable = [
+        'name_team',
+        'city_team',
+        'id_category',
+        'id_trainer',
+        'id_delegate',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'id_category', 'id_category');
+    }
+
+    public function trainer()
+    {
+        return $this->belongsTo(Trainer::class, 'id_trainer', 'id_trainer');
+    }
+
+    public function delegate()
+    {
+        return $this->belongsTo(Delegate::class, 'id_delegate', 'id_delegate');
+    }
 }
